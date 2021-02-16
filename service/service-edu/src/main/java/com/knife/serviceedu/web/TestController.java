6e3b9f1f287af3bdc9bc0d882fba1dd414ed93e2
VERLAAT <787521074@qq.com>
2021-02-16 15:06:00 +0800

    示例

--- service/service-edu/src/main/java/com/knife/serviceedu/web/TestController.java
+++ service/service-edu/src/main/java/com/knife/serviceedu/web/TestController.java
@@ -2,9 +2,14 @@ package com.knife.serviceedu.web;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotBlank;
 
 /**
  * @author Mr_W
@@ -19,6 +24,12 @@ public class TestController {
     @GetMapping
     @ApiOperation("测试接口")
     public String test() {
+        return "test";
+    }
+
+    @GetMapping
+    @ApiOperation("测试接口")
+    public String test(@RequestParam("test") @ApiParam("测试") @NotBlank String test) {
         return "test";
     }
 
