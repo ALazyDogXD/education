7397fd11bfd6d3b3430520edbd3a398ed4878aa3
VERLAAT <787521074@qq.com>
2021-02-16 13:43:48 +0800

    初始化项目

---
+++ service/service-edu/src/main/java/com/knife/serviceedu/ServiceEduApplication.java
@@ -0,0 +1,17 @@
+package com.knife.serviceedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mr_W
+ * @date 2021/2/16 11:46
+ */
+@SpringBootApplication(scanBasePackages = "com.knife")
+public class ServiceEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceEduApplication.class, args);
+    }
+
+}
---
+++ service/service-edu/src/main/java/com/knife/serviceedu/web/TestController.java
@@ -0,0 +1,25 @@
+package com.knife.serviceedu.web;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Mr_W
+ * @date 2021/2/16 13:19
+ * @description: controller 测试
+ */
+@RestController
+@RequestMapping("test")
+@Api("测试")
+public class TestController {
+
+    @ApiOperation("测试接口")
+    @GetMapping
+    public String test() {
+        return "test";
+    }
+
+}
