0df26eaf39e3e2ac187b6fe6bd59620b001113e8
jsyzgyz <49144501+jsyzgyz@users.noreply.github.com>
2021-02-17 10:48:45 +0800

    教师功能接口开发

--- service/service-edu/src/main/java/com/knife/serviceedu/service/EduTeacherService.java
+++ service/service-edu/src/main/java/com/knife/serviceedu/service/EduTeacherService.java
@@ -1,7 +1,12 @@
 package com.knife.serviceedu.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.knife.serviceedu.domain.entity.EduTeacherDO;
+
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +17,86 @@ import com.knife.serviceedu.domain.entity.EduTeacherDO;
  * @since 2021-02-16
  */
 public interface EduTeacherService extends IService<EduTeacherDO> {
+    /**
+     * @author 风烛
+     * @date 2021年2月16日 16:12
+     * @Description 新增一名教师
+     * @Param * @param teacher:老师类
+     * @return: int
+     **/
+    int addTeacher(EduTeacherDO teacher);
+
+    /**
+     * @author 风烛
+     * @date 2021年2月16日 16:23
+     * @Description 删除一名教师
+     * @Param * @param id:教师id
+     * @return: int
+     **/
+    int deleteTeacher(int id);
+
+    /**
+     * @author 风烛
+     * @date 2021年2月16日 21:01
+     * @Description 根据id删除一组教师
+     * @Param * @param ids: 教师数组
+     * @return: int
+     **/
+    int deleteTeachers(List<String> ids);
+
+    /**
+     * @author 风烛
+     * @date 2021年2月16日 21:04
+     * @Description 根据特定条件进行删除(where)
+     * @Param * @param map:where条件键值对
+     * @return: int
+     **/
+    int deleteByMap(HashMap map);
+
+    /**
+     * @author 风烛
+     * @date 2021年2月16日 21:24
+     * @Description 根据id对教师数据进行修改
+     * @Param * @param teacher:  教师类
+     * @return: int
+     **/
+    int updateTeacherById(EduTeacherDO teacher);
+
+    /**
+     * @author 风烛
+     * @date 2021年2月16日 21:31
+     * @Description 根据id查找教师
+     * @Param * @param id:  教师id
+     * @return: com.knife.serviceedu.domain.entity.EduTeacherDO
+     **/
+    EduTeacherDO selectByTeacher(int id);
+
+    /**
+     * @author 风烛
+     * @date 2021年2月16日 21:33
+     * @Description 根据id集合查找一组教师
+     * @Param * @param ids:  教师id集合
+     * @return: java.util.List<com.knife.serviceedu.domain.entity.EduTeacherDO>
+     **/
+    List<EduTeacherDO> selectByTeachers(List<String> ids);
+
+    /**
+     * @author 风烛
+     * @date 2021年2月16日 21:37
+     * @Description 根据map条件进行查询
+     * @Param * @param map:  查询的map
+     * @return: java.util.List<com.knife.serviceedu.domain.entity.EduTeacherDO>
+     **/
+    List<EduTeacherDO> selectTeacherMap(HashMap map);
+
+    /**
+     * @author 风烛
+     * @date 2021年2月16日 22:02
+     * @Description 无条件分页查询
+     * @Param * @param page:  分页数据
+     * @return: com.baomidou.mybatisplus.core.metadata.IPage<com.knife.serviceedu.domain.entity.EduTeacherDO>
+     **/
+    IPage<EduTeacherDO> selectTeacherPage(Page page);
+
 
 }
--- service/service-edu/src/main/java/com/knife/serviceedu/service/impl/EduTeacherServiceImpl.java
+++ service/service-edu/src/main/java/com/knife/serviceedu/service/impl/EduTeacherServiceImpl.java
@@ -1,10 +1,17 @@
 package com.knife.serviceedu.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.knife.serviceedu.domain.entity.EduTeacherDO;
 import com.knife.serviceedu.mapper.EduTeacherMapper;
 import com.knife.serviceedu.service.EduTeacherService;
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +23,52 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class EduTeacherServiceImpl extends ServiceImpl<EduTeacherMapper, EduTeacherDO> implements EduTeacherService {
+
+
+    @Override
+    public int addTeacher(EduTeacherDO teacher){
+        return baseMapper.insert(teacher);
+    }
+
+    @Override
+    public int deleteTeacher(int id) {
+        return baseMapper.deleteById(id);
+    }
+
+    @Override
+    public int deleteTeachers(List<String> ids) {
+        return baseMapper.deleteBatchIds(ids);
+    }
+
+    @Override
+    public int deleteByMap(HashMap map) {
+        return baseMapper.deleteByMap(map);
+    }
+
+    @Override
+    public int updateTeacherById(EduTeacherDO teacher) {
+        return baseMapper.updateById(teacher);
+    }
+
+    @Override
+    public EduTeacherDO selectByTeacher(int id) {
+        return baseMapper.selectById(id);
+    }
+
+    @Override
+    public List<EduTeacherDO> selectByTeachers(List<String> ids) {
+        return baseMapper.selectBatchIds(ids);
+    }
+
+    @Override
+    public List<EduTeacherDO> selectTeacherMap(HashMap map) {
+        return baseMapper.selectByMap(map);
+    }
+
+    @Override
+    public IPage<EduTeacherDO> selectTeacherPage(Page page) {
+        return baseMapper.selectPage(page, null);
+    }
+
 
 }
